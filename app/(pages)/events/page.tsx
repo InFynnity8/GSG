e590aa5158e2194a367b3478d638406009149e6d
InFynnity8 <sackeyemmanuelfynn@gmail.com>
2025-11-24 19:22:05 +0000

    feat: Add new pages

---
+++ app/(pages)/events/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Events = () => {
+  return (
+    <div className='min-h-[75vh]'>Events</div>
+  )
+}
+
+export default Events
---
+++ app/(pages)/give/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Donate = () => {
+  return (
+    <div className='min-h-[75vh]'>Donate</div>
+  )
+}
+
+export default Donate

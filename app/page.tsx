599a183c0f3d92e70476caa98d1744837fb81fd5
InFynnity8 <sackeyemmanuelfynn@gmail.com>
2026-03-04 14:05:01 +0000

    chore: Animations added to home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,24 @@ import About from "@/components/sections/About";
 import UpcomingEvents from "@/components/sections/UpcomingEvents";
 import Give from "@/components/sections/Give";
 import Values from "@/components/sections/Values";
+import { ScrollReveal } from "@/components/ui/scroll-reveal";
 
 export default function Home() {
   return (
     <div className="">
       <HeroCarousel />
-      <About />
-      <UpcomingEvents />
-      <Values/>
-      <Give />
+      <ScrollReveal direction="up" distance={50} delay={200}>
+        <About />
+      </ScrollReveal>
+      <ScrollReveal direction="up" distance={40} delay={100}>
+        <UpcomingEvents />
+      </ScrollReveal>
+      <ScrollReveal direction="up" distance={50} delay={200}>
+        <Values />
+      </ScrollReveal>
+      <ScrollReveal direction="up" distance={40} delay={100}>
+        <Give />
+      </ScrollReveal>
     </div>
   );
 }
